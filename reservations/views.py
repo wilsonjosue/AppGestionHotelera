2f9788e1c6ede84955f0cc7c4ff74e2ebedaa282
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-01 14:42:38 -0500

    Se implementa GuestViewSet para la API y obtiene todos los objetos Guest de la base de datos. Cambio realizado en reservations/views.py

--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from .models import Guest, Room, Reservation
 # Create your views here.
+
+class GuestViewSet(viewsets.ModelViewSet):
+    queryset = Guest.objects.all()
+
 
