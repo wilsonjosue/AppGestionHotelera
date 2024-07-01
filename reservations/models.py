6f1d9c23e239cc53f0a7709e2e2e4f5e0d82e2a0
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-01 13:02:18 -0500

    Definimos e implementamos el modelo para la habitacion(Room). Cambio realizado en reservations/models.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Guest(models.Model):
+class Guest(models.Model):# Modelo para el invitado (cliente)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
+
+class Room(models.Model):# Modelo para la habitacion del(cliente)
+    room_number = models.CharField(max_length=10)
+    room_type = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+    
+
