8f9c00aa1160ec16c657b7d7ec5d0c005e9790b5
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-01 13:26:14 -0500

    Definimos e implementamos el modelo para Reserva(Reservation). Cambio realizado en reservations/models.py

--- reservations/models.py
+++ reservations/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 # Create your models here.
-class Guest(models.Model):# Modelo para el invitado (cliente)
+
+class Guest(models.Model):# Modelo para el invitado (cliente) Tabla
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.EmailField()
+    email = models.EmailField()# email valido
 
-class Room(models.Model):# Modelo para la habitacion del(cliente)
-    room_number = models.CharField(max_length=10)
-    room_type = models.CharField(max_length=50)
-    is_available = models.BooleanField(default=True)
-    
+class Room(models.Model):# Modelo para la habitacion del(cliente) Tabla
+    room_number = models.CharField(max_length=10)# Numero de habitacion
+    room_type = models.CharField(max_length=50)# Tipo de habitacion
+    is_available = models.BooleanField(default=True)# Si esta disponible
+
+class Reservation(models.Model):# Modelo Reserva: Relaciona con las otras Tablas
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)# Relacion muchos a uno 
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)# Relacion muchos a uno
+    check_in = models.DateField()#La fecha de entrada
+    check_out = models.DateField()#Para la fecha de salida
+
+
 
