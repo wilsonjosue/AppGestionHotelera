1adda482c5a24dedc02fd52229b0c9db6c578002
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-01 15:14:08 -0500

    Se configura las rutas de URL para la API donde  DefaultRouter simplifica la creación de rutas para los viewsets. Cambio realizado en reservations/urls.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,3 +2,11 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GuestViewSet, RoomViewSet, ReservationViewSet
 
+router = DefaultRouter()# Automáticamente maneja la configuración de rutas para los viewsets.
+router.register(r'guests', GuestViewSet)#Registra cada viewset con el router
+router.register(r'rooms', RoomViewSet)
+router.register(r'reservations', ReservationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),# Incluye todas las rutas generadas por el DefaultRouter
+]
