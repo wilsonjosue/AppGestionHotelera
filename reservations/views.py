7b7879808aa689b5cb4827b834fe3a720c8a675d
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 16:38:06 -0500

    Especifica el serializador que debe ser utilizado para este viewset.. Cambio realizado en reservations/views.py

--- reservations/views.py
+++ reservations/views.py
@@ -5,11 +5,13 @@ from .serializers import GuestSerializer, RoomSerializer, ReservationSerializer
 # Create your views here.
 
 class GuestViewSet(viewsets.ModelViewSet):
-    queryset = Guest.objects.all()
-
+    queryset = Guest.objects.all()# El conjunto de datos que debe de ser devuelto 
+    serializer_class = GuestSerializer# Especifica el serializador para este viewset.
 
 class RoomViewSet(viewsets.ModelViewSet):
-    queryset = Room.objects.all()
+    queryset = Room.objects.all()# El conjunto de datos que debe de ser devuelto 
+    serializer_class = RoomSerializer# Especifica el serializador para este viewset.
 
 class ReservationViewSet(viewsets.ModelViewSet):
-    queryset = Reservation.objects.all()
+    queryset = Reservation.objects.all()# El conjunto de datos que debe de ser devuelto 
+    serializer_class = ReservationSerializer# Especifica el serializador para este viewset.
