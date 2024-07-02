f01a198b5a41daefd430813246f620801c593970
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 16:45:53 -0500

    Se especifica en los comentarios la logica relacionada para entrada y salida de datos. Cambio realizado en reservations/views.py

--- reservations/views.py
+++ reservations/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from .models import Guest, Room, Reservation
 from .serializers import GuestSerializer, RoomSerializer, ReservationSerializer
 # Create your views here.
+#Agrupan la lógica relacionada con las vistas en conjuntos, proporcionando acciones CRUD 
+#predeterminadas y utilizando los serializadores para manejar la entrada y salida de datos.
 
 class GuestViewSet(viewsets.ModelViewSet):
     queryset = Guest.objects.all()# El conjunto de datos que debe de ser devuelto 
