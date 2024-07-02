82ed6d8df4c2dde2183a0c991648800cca2c1a1d
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 16:23:33 -0500

    Se crear un serializador basado en un modelo Django para Room. Cambio realizado en reservations/serializers.py

--- reservations/serializers.py
+++ reservations/serializers.py
@@ -5,3 +5,9 @@ class GuestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Guest
         fields = '__all__'
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
