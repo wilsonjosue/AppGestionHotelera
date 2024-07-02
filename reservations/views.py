cf68fc946b380bfe605e7aa41304191b88f4e09e
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 16:29:18 -0500

    Importamos los serializadores correspondientes para Guest, Room, y Reservation. Cambio realizado en reservations/views.py

--- reservations/views.py
+++ reservations/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Guest, Room, Reservation
+from .serializers import GuestSerializer, RoomSerializer, ReservationSerializer
 # Create your views here.
 
 class GuestViewSet(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
+
 
 class RoomViewSet(viewsets.ModelViewSet):
     queryset = Room.objects.all()
