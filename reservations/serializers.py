3c7f0aa440de6e40916167237dbf6ad1860b1900
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 16:22:26 -0500

    Se crear un serializador basado en un modelo Django para Guest . Cambio realizado en  reservations/serializers.py

--- reservations/serializers.py
+++ reservations/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Guest, Room, Reservation
+
+class GuestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Guest
+        fields = '__all__'
