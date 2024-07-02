58bd6a02b6a963aac875656d19a1d05624cde679
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-02 17:27:52 -0500

    Se añade la ruta para el index . Cambio realizado en reservations/urls.py

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import GuestViewSet, RoomViewSet, ReservationViewSet
+from . import views
 
 router = DefaultRouter()# Automáticamente maneja la configuración de rutas para los viewsets.
 router.register(r'guests', GuestViewSet)#Registra cada viewset con el router
@@ -9,4 +10,5 @@ router.register(r'reservations', ReservationViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),# Incluye todas las rutas generadas por el DefaultRouter
+    path("index", views.index,name="index")
 ]
