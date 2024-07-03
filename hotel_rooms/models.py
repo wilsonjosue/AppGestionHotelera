232f77895b37d64df84e060357381c7e6cd6ae88
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-03 17:53:17 -0500

    Se crea una app para manejar los items del hotel para las habitaciones y se implementa el modelo HotelRoom. Cambio realizado hotel_rooms/index.html

---
+++ hotel_rooms/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class HotelRoom(models.Model):
+    ROOM_TYPES = [
+        ('Suite Room', 'Suite Room'),
+        ('King Room', 'King Room'),
+        ('Family Room', 'Family Room'),
+        ('Deluxe Room', 'Deluxe Room'),
+    ]
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    room_type = models.CharField(max_length=20, choices=ROOM_TYPES)
+
+    def __str__(self):
+        return f"{self.name} - {self.room_type} - ${self.price}"
