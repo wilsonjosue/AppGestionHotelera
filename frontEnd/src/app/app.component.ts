aa900c9c528ecff0ca4471d385d8f812cd4842c0
Sergio Danilo Hancco Mullisaca <shanccom@unsa.edu.pe>
2024-07-06 15:46:41 -0500

    Configuracion en el archivo app.config.ts - Se incluyo provideHttpClient

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GuestComponent } from './component/guest/guest.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GuestComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontEnd';
+  title = 'frontend';
 }
