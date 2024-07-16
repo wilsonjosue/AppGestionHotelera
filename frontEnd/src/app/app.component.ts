cd73215f40471a59a8f7015395729547256ba85d
wilsonjosue <wturpoh@unsa.edu.pe>
2024-07-16 09:52:30 -0500

    Se añade verifica la configuracion de html component con relacion a la pagina principal

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GuestsComponent } from './component/guest/guest.component';
 import { LoginComponent } from './component/login/login.component';
+import { PaginaPrincipalComponent } from './component/pagina-principal/pagina-principal.component';
+//import { PaginaPrincipalComponent } from './component/login/login.component';
+//import { LoginComponent } from './component/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GuestsComponent, LoginComponent],
+  imports: [RouterOutlet, GuestsComponent, LoginComponent, PaginaPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
